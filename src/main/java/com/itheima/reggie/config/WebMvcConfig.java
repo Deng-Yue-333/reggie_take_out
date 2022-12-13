da6b58503a7b07f29eb0870cbecb8c589e56850f
Dylan3364 <54977633+Dylan3364@users.noreply.github.com>
2022-12-13 21:38:30 +0800

    2

--- src/main/java/com/itheima/reggie/config/WebMvcConfig.java
+++ src/main/java/com/itheima/reggie/config/WebMvcConfig.java
@@ -1,4 +1,4 @@
-package com.itheima.config;
+package com.itheima.reggie.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
@@ -7,11 +7,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 
 @Slf4j
 @Configuration
-public class WebMcvcConfig extends WebMvcConfigurationSupport {
-
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+    /**
+     * 设置静态资源映射
+     * @param registry
+     */
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        log.info("开始静态资源映射...");
+        log.info("开始进行静态资源映射...");
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
     }
